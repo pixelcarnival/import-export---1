b4d6b5ca70e013b0b57587477bcbb1f6dd21478c
pixelcarnival <112376989+pixelcarnival@users.noreply.github.com>
2022-11-26 09:10:37 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,19 +10,3 @@ export default function initApp() {
     document.body.style.backgroundColor = getRandomColor()
   })
 }
-// export default initApp
-
-// import { getRandomColor } from './utils'
-
-// function initApp() {
-//   const button = document.createElement('button')
-//   button.classList.add('button')
-//   button.textContent = 'Изменить цвет страницы'
-//   document.body.append(button)
-
-//   button.addEventListener('click', () => {
-//     document.body.style.backgroundColor = getRandomColor()
-//   })
-// }
-
-// export default initApp
